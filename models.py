acfce70448049d82604a51df764fd0c37ac737bb
WeiweiGu <949711497@qq.com>
2019-11-07 15:56:55 -0500

    Update models.py

--- models.py
+++ models.py
@@ -12,21 +12,23 @@ import torch
 from torch.autograd import Variable
 
 class GAT(nn.Module):
-    def __init__(self, K, node_num, nfeat, nhid,  sample_size, dropout, logistic_dim):
+    def __init__(self,K,node_num, nfeat, nhid, nclass, sampleSize, dropout):
         super(GAT, self).__init__()
-        self.gc1 = GraphConvolution(K, node_num, nfeat, nhid, sample_size[1], 'False','True')
-        self.gc2 = GraphConvolution(1, node_num, K*nhid, logistic_dim, sample_size[0], 'False', 'False')
-        self.gc3 = LogisticRegression(logistic_dim, 1)
+        self.gc1 = GraphConvolution(K, node_num, nfeat, nhid, sampleSize[1],'False','True')
+        self.gc2 = GraphConvolution(1, node_num, K*nhid, 14*nclass, sampleSize[0],'False','False')
+        #self.gc3 = GraphConvolution(1, node_num, 4*7*nclass, 7*nclass, 'False','False')
+        self.gc6 = LogisticRegression(14*nclass,1)
         self.dropout = dropout
 
-    def forward(self, nodes, node_neis, node_second_neis):
-        node_nei_features = self.gc1(node_neis,node_second_neis)
-        node_nei_features = F.dropout(node_nei_features, self.dropout, training=self.training)
-        feature_num = node_nei_features.size()[1]
-        features = range(feature_num)
-        features_tensor = Variable(torch.LongTensor(features).cuda())
-        node_features = torch.index_select(nodes,1,features_tensor)
-        _node_features = self.gc2(node_features, node_nei_features)
-        _node_features = F.dropout(_node_features, self.dropout, training=self.training)
-        edge_features = self.gc3(_node_features)
-        return edge_features
+    def forward(self, x, x_nei, x_nei2):
+#        x = F.dropout(x, self.dropout, training=self.training)
+        x_1 = self.gc1(x_nei,x_nei2)
+        x_1 = F.dropout(x_1, self.dropout, training=self.training)
+        nei_, feature_size = x_1.size()
+        feature_size_list = range(feature_size)
+        feature_size_tensor = Variable(torch.LongTensor(feature_size_list).cuda())
+        x_features = torch.index_select(x,1,feature_size_tensor)
+        x_2 = self.gc2(x_features, x_1)
+        x_2 = F.dropout(x_2, self.dropout, training=self.training)
+        x_6 = self.gc6(x_2)
+        return x_6
